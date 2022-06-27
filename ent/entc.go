c34906604241ecf05a2fba1b9dfd5a3ab1c4c6f3
Oussama Sethoum <os.sethoum@gmail.com>
2022-06-27 12:06:19 +0100

    second commit

--- ent/entc.go
+++ ent/entc.go
@@ -14,6 +14,7 @@ import (
 func main() {
 	ex, err := entgql.NewExtension(
 		entgql.WithWhereFilters(true),
+		entgql.WithMutationInput(),
 		entgql.WithConfigPath("../gqlgen.yml"),
 		entgql.WithSchemaGenerator(),
 		entgql.WithSchemaPath("../graph/schemas/schema.graphqls"),
